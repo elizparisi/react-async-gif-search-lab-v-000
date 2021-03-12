f38bb6a973e84d19552d1aaa8913d4807469d23a
Liz P <parisielizabeth@gmail.com>
2021-03-12 08:15:11 +0000

    Automatically backed up by Learn

--- src/components/GifList.js
+++ src/components/GifList.js
@@ -7,6 +7,8 @@ class GifList extends React.Component {
     return(
       <ul>
         <li>img src={this.props.gifs[0]}</li>
+        <li>img src={this.props.gifs[1]}</li>
+        <li>img src={this.props.gifs[2]}</li>
       </ul>
       )
   }
