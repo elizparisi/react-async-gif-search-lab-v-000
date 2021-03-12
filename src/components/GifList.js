2f77f6309b2c836397fc0f47cc6bd475d7b194c5
Liz P <parisielizabeth@gmail.com>
2021-03-12 08:12:10 +0000

    Automatically backed up by Learn

---
+++ src/components/GifList.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class GifList extends React.Component {
+  
+  
+  render() {
+    return(
+      <ul>
+        <li> </li>
+      </ul>
+      )
+  }
+}
+
+export default GifList
